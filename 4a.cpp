3e2e0ede31535bc0890fe95eea0373628c2f9098
vahrelius <vahrelius@gmail.com>
2021-02-26 22:06:59 +0700

    4a

---
+++ 4a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+
+
+    if (number % 2 == 0 && number != 2) {
+        cout << "YES" << endl;
+    }else {
+        cout << "NO" << endl;
+    }
+} 
